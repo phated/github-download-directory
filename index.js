cdd23796cbd5d427477ad2f2e72b48d1f9e5d837
Blaine Bublitz <blaine@tidelift.com>
2020-06-05 23:20:22 -0700

    Breaking: Refactor to add cache & auth support (#4)
    * New: Add persistent-cache for github tree to avoid rate limit
    
    * Update: Improve cache logic & test it
    
    * Build: Add eslint
    
    * Build: Ensure cache is written
    
    * Build: Remove coveralls
    
    * Breaking: Huge rewrite to use constructor
    
    * New: Add GitHub token support (ref #5)
    
    * Build: Fix tests
    
    Co-authored-by: Cesar Salazar <csalazar@devsu.com>

--- index.js
+++ index.js
@@ -4,24 +4,22 @@ var fs = require('fs');
 var { dirname } = require('path');
 var { promisify } = require('util');
 
-var ghTree = require('github-trees');
-var GithubContent = require('github-content');
-var mkdirp = require('fs-mkdirp-stream/mkdirp');
+var Keyv = require('keyv');
+var { Octokit } = require('@octokit/rest');
 
-function getFiles(owner, repo, branch, paths) {
-  var client = new GithubContent({ owner, repo, branch });
+var mkdir = promisify(fs.mkdir);
+var writeFile = promisify(fs.writeFile);
 
-  // Call with client as `this`
-  return promisify(client.files).call(client, paths);
-}
+var ONE_HOUR_IN_MS = 1000 * 3600;
+
+var defaultCacheOpts = {
+  ttl: ONE_HOUR_IN_MS,
+  namespace: 'github-download-directory'
+};
 
 async function createDirectories(filepath) {
   var dir = dirname(filepath);
-  return promisify(mkdirp).call(null, dir);
-}
-
-async function writeFile(filepath, contents) {
-  return promisify(fs.writeFile).call(null, filepath, contents);
+  return mkdir(dir, { recursive: true });
 }
 
 async function output(file) {
@@ -29,22 +27,61 @@ async function output(file) {
   await writeFile(file.path, file.contents);
 }
 
-async function fetchFiles(owner, repo, directory, options = {}) {
-  var { tree } = await ghTree(owner, repo, { recursive: true, sha: options.sha });
+class Downloader {
+  constructor(options = {}) {
+    var cacheOpts = Object.assign({}, defaultCacheOpts, options.cache);
 
-  var paths = tree
-    .filter((node) => node.path.startsWith(directory) && node.type === 'blob')
-    .map((node) => node.path);
+    this.cache = new Keyv(cacheOpts);
 
-  return getFiles(owner, repo, options.sha, paths);
+    this._octokit = new Octokit(options.github);
+  }
+
+  async getTree(owner, repo, options = {}) {
+    var sha = options.sha || 'master';
+    var cacheKey = `${owner}/${repo}#${sha}`;
+
+    var cachedTree = await this.cache.get(cacheKey);
+    if (cachedTree) {
+      return cachedTree
+    }
+
+    var { data: { tree } } = await this._octokit.git.getTree({
+      owner,
+      repo,
+      tree_sha: sha,
+      recursive: true
+    });
+
+    await this.cache.set(cacheKey, tree);
+
+    if (typeof this.cache.save === 'function') {
+      await this.cache.save();
+    }
+
+    return tree;
+  }
+
+  async fetchFiles(owner, repo, directory, options = {}) {
+    var tree = await this.getTree(owner, repo, options);
+
+    var files = tree
+      .filter((node) => node.path.startsWith(directory) && node.type === 'blob')
+      .map(async (node) => {
+        var { data } = await this._octokit.git.getBlob({ owner, repo, file_sha: node.sha });
+        return {
+          path: node.path,
+          contents: Buffer.from(data.content, data.encoding),
+        };
+      });
+
+    return Promise.all(files);
+  }
+
+  async download(owner, repo, directory, options = {}) {
+    var files = await this.fetchFiles(owner, repo, directory, options);
+    return Promise.all(files.map(output));
+  }
 }
 
-async function download(owner, repo, directory, options = {}) {
-  var files = await fetchFiles(owner, repo, directory, options);
-
-  await Promise.all(files.map(output));
-}
-
-module.exports = download;
-module.exports.fetchFiles = fetchFiles;
-module.exports.output = output;
+module.exports = new Downloader();
+module.exports.Downloader = Downloader;
